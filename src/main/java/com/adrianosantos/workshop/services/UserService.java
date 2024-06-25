a6187c43f4985fa3e9df59b576c66be2cce886ec
Adriano Santos <adrianogss09@gmail.com>
2024-06-24 21:05:53 -0300

    Obtendo um usuário por ID

--- src/main/java/com/adrianosantos/workshop/services/UserService.java
+++ src/main/java/com/adrianosantos/workshop/services/UserService.java
@@ -1,12 +1,14 @@
 package com.adrianosantos.workshop.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.adrianosantos.workshop.domain.User;
 import com.adrianosantos.workshop.repository.UserRepository;
+import com.adrianosantos.workshop.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -20,7 +22,10 @@ public class UserService {
 		
 	}
 	
-	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
 	
 	
 	
