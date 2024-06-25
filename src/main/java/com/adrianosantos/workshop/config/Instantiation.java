f5180d837f2e86ff63e0b3dd7056fb310021c848
Adriano Santos <adrianogss09@gmail.com>
2024-06-25 15:42:48 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/com/adrianosantos/workshop/config/Instantiation.java
+++ src/main/java/com/adrianosantos/workshop/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.adrianosantos.workshop.domain.Post;
 import com.adrianosantos.workshop.domain.User;
+import com.adrianosantos.workshop.dto.AuthorDTO;
 import com.adrianosantos.workshop.repository.PostRepository;
 import com.adrianosantos.workshop.repository.UserRepository;
 
@@ -35,10 +36,11 @@ public class Instantiation implements CommandLineRunner {
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
-
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
 	}
