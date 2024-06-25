644cab1010dc0e83d7fb9d5772193d5e1dca969f
Adriano Santos <adrianogss09@gmail.com>
2024-06-25 14:39:14 -0300

    Deleção de usuário com DELETE

--- src/main/java/com/adrianosantos/workshop/resources/UserResource.java
+++ src/main/java/com/adrianosantos/workshop/resources/UserResource.java
@@ -42,7 +42,11 @@ public class UserResource {
 		return ResponseEntity.created(uri).build();
 	}
 	
-	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> delete(@PathVariable String id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
 	
 	
 	
