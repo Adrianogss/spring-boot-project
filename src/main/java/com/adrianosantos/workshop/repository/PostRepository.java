5a5cf6e7d569cb86603b5df89186f32062904b0f
Adriano Santos <adrianogss09@gmail.com>
2024-06-25 18:30:10 -0300

    Consulta simples com query methods

--- src/main/java/com/adrianosantos/workshop/repository/PostRepository.java
+++ src/main/java/com/adrianosantos/workshop/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.adrianosantos.workshop.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,7 +10,7 @@ import com.adrianosantos.workshop.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
-	
+	List<Post> findByTitleContainingIgnoreCase(String text);
 	
 	
 	
--- src/main/java/com/adrianosantos/workshop/resources/PostResource.java
+++ src/main/java/com/adrianosantos/workshop/resources/PostResource.java
@@ -1,13 +1,12 @@
 package com.adrianosantos.workshop.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 import com.adrianosantos.workshop.domain.Post;
+import com.adrianosantos.workshop.resources.util.URL;
 import com.adrianosantos.workshop.services.PostService;
 
 @RestController
@@ -23,5 +22,15 @@ public class PostResource {
 		Post obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
+	
+	@GetMapping("/titlesearch")
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String text){
+		text = URL.decodeParam(text);
+		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
+	}
+	
+	
+	
 	
 }
